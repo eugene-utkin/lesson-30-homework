c770d34d119b1b841698108f3f3bf03d50845095
Evgeniy_Utkin <eugene.utkin@gmail.com>
2020-09-01 11:09:39 +0500

    All Done

--- app.rb
+++ app.rb
@@ -3,88 +3,77 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
-def init_db
-  @db = SQLite3::Database.new 'blogposts.db'
-  @db.results_as_hash = true
+set :database, {adapter: "sqlite3", database: "blogposts.db"}
+
+class Post < ActiveRecord::Base
+  validates :post, presence: true, length: { minimum: 3 }
+  validates :author, presence: true
+end
+
+class Comment < ActiveRecord::Base
+  validates :message, presence: true, length: { minimum: 3 }
 end
 
 before do
-  init_db
-end
-
-configure do
-  init_db
-  @db.execute 'CREATE TABLE IF NOT EXISTS Posts
-  (
-    "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-    "created_date" DATE,
-    "content" TEXT,
-    "author" TEXT
-  )'
-  @db.execute 'CREATE TABLE IF NOT EXISTS Comments
-  (
-    "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-    "created_date" DATE,
-    "comment" TEXT,
-    "post_id" INTEGER
-  )'
+  @posts = Post.all
+  @comments = Comment.all
 end
 
 get '/' do
-  @results = @db.execute 'SELECT * FROM Posts ORDER BY id DESC'
+  @results = Post.order('created_at DESC')
   erb :index 
 end
 
 get '/new' do
+  @c = Post.new
   erb :new
 end
 
 post '/new' do
+
+  @c = Post.new params[:post]
+    if @c.save
+      erb "<h2>Спасибо, вы опубликовали пост!</h2>"
+      redirect to '/'
+    else
+      @error = @c.errors.full_messages.first
+      erb :new
+ end
+
   
-  @content = params[:content]
-  @author = params[:author]
-
-  hh = {  :author => 'Type author!',
-          :content => 'Type text!'}
-
-@error = hh.select {|key,_| params[key] == ""}.values.join("<br />")
-
-
-  if @error != ''
-    return erb :new
-  end
-
-  @db.execute 'insert into Posts (created_date, content, author) values (datetime(), ?, ?)', [@content, @author]
-
-  redirect to '/'
 end
 
 get '/details/:post_id' do
+
+  
+  
   post_id = params[:post_id]
 
-  results = @db.execute 'SELECT * FROM Posts WHERE id = ?', [post_id]
+  results = Post.where(id: post_id)
   @row = results[0]
 
-  @comments = @db.execute 'SELECT * FROM Comments WHERE post_id = ? ORDER BY id', [post_id]
+  @commentresults = Comment.where(post: post_id).order('created_at DESC')
+
+  @d = Comment.new
 
   erb :details
 end
 
 post '/details/:post_id' do
   post_id = params[:post_id]
-  content = params[:content]
-  author = params[:author]
-
-  if content.length <= 0
-    @error = 'Type comment!'
-    results = @db.execute 'SELECT * FROM Posts WHERE id = ?', [post_id]
+  results = Post.where(id: post_id)
   @row = results[0]
 
-  @comments = @db.execute 'SELECT * FROM Comments WHERE post_id = ? ORDER BY id', [post_id]
-    return erb :details
-  end
+  @commentresults = Comment.where(post: post_id).order('created_at DESC')
 
-  @db.execute 'insert into Comments (created_date, comment, post_id) values (datetime(), ?, ?)', [content, post_id]
+  @d = Comment.new params[:comment]
+  @d.post = post_id
+    if @d.save
+      erb "<h2>Спасибо за комментарий!</h2>"
+      redirect to ('/details/' + post_id)
+    else
+      @error = @d.errors.full_messages.first
+      erb :details
+ end
 
-  redirect to ('/details/' + post_id)
 end
---
+++ db/migrate/20200901043113_create_posts.rb
@@ -0,0 +1,10 @@
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+  	create_table :posts do |t|
+        t.text :post
+        t.text :author
+
+        t.timestamps
+end
+  end
+end
